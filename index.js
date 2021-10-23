55c079d44111c2a70e9dcedebb4235d8cdeb7958
The Captain <stepfightgamingyt@gmail.com>
2021-10-23 20:57:28 +0200

    yes

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const { Plugin } = require('powercord/entities');
 
-module.exports = class Template extends Plugin {
+module.exports = class AOUUutilities extends Plugin {
+    id;
     startPlugin() {
-        return;
+        powercord.api.commands.registerCommand({
+            command: 'aou-ban',
+            description: 'ban a user for a reason',
+            usage: '{c} <id> <reason>',
+            executor: (args) => {
+            let id = args[0];
+            args.shift();
+            return {
+                send: true,
+                result: `<@850025674093494303> ban ${id} ${args.join(" ")}`
+            }
+            },
+            autocomplete: (args) => {
+                if (args[1] === void 0) {
+                    return false;
+                }
+                if (args[2] === void 0) {
+                    return {
+                        header: 'reasons',
+                        commands: [
+                            {
+                                command: "Scam; Your account may be hacked, please change your password.",
+                                description: "discord/steam scam"
+                            },
+                            {
+                                command: "NSFW",
+                                description: "NSFW"
+                            }
+                        ]
+                    };
+                }
+            }
+          });
     }
     pluginWillUnload() {
-        return;
+        powercord.api.commands.unregisterCommand("aou-ban")
     }
 };
